5ad6de84fc01868c5369dbdfe3865612860a947b
chaudharinikita1234 <140835931+chaudharinikita1234@users.noreply.github.com>
2025-10-18 17:52:23 +0530

    update example.js with new function implementation

--- example.js
+++ example.js
@@ -1,35 +1,58 @@
 import dotenv from "dotenv";
-import { tts, stt, merge } from "./index.js";
+import { tts, stt, merge, llm } from "./index.js";
 
 dotenv.config();
 
 // Deepgram TTS Example
-tts({
-  provider: "deepgram",
-  apiKey: process.env.DEEPGRAM_API_KEY,
-  text: "Hello! This is a test using the Deepgrams's Aura-2 tts model.",
-  voice: "aura-2-luna-en",
-  outputFile: "output_1.mp3",
+// tts({
+//   provider: "deepgram",
+//   apiKey: process.env.DEEPGRAM_API_KEY,
+//   text: "Hello! This is a test using the Deepgrams's Aura-2 tts model.",
+//   voice: "aura-2-luna-en",
+//   outputFile: "output_1.mp3",
+// });
+
+// tts({
+//   provider: "deepgram",
+//   apiKey: process.env.DEEPGRAM_API_KEY,
+//   text: "This voice is called Luna.",
+//   voice: "aura-2-luna-en",
+//   outputFile: "output_2.mp3",
+// });
+
+// // Deepgram STT Example
+// stt({
+//   provider: "deepgram",
+//   apiKey: process.env.DEEPGRAM_API_KEY,
+//   audioFile: "https://dpgr.am/spacewalk.wav",
+//   outputFile: "transcription.json",
+// });
+
+// // File merge Example [First there should be two audio files for this to work]
+// merge({
+//   inputFiles: ["output_1.mp3", "output_2.mp3"],
+//   outputFile: "Combined.mp3",
+// });
+
+// OpenAI LLM Example (uncomment to use)
+// llm({
+//   provider: "openai",
+//   apiKey: process.env.OPENAI_API_KEY,
+//   text: "Write in short about pytorch",
+//   model: "gpt-4o-mini",
+//   temperature: 0.8,
+// }).then((response) => {
+//   console.log("Generated text:", response);
+// });
+
+// Google Gemini LLM Example (uncomment to use)
+llm({
+  provider: "gemini",
+  apiKey: process.env.GEMINI_API_KEY,
+  text: "Write in short about pytorch",
+  model: "gemini-2.0-flash-exp",
+  temperature: 0.8,
+}).then((response) => {
+  console.log("Generated text:", response);
 });
 
-tts({
-  provider: "deepgram",
-  apiKey: process.env.DEEPGRAM_API_KEY,
-  text: "This voice is called Luna.",
-  voice: "aura-2-luna-en",
-  outputFile: "output_2.mp3",
-});
-
-// Deepgram STT Example
-stt({
-  provider: "deepgram",
-  apiKey: process.env.DEEPGRAM_API_KEY,
-  audioFile: "https://dpgr.am/spacewalk.wav",
-  outputFile: "transcription.json",
-});
-
-// File merge Example [First there should be two audio files for this to work]
-merge({
-  inputFiles: ["output_1.mp3", "output_2.mp3"],
-  outputFile: "Combined.mp3",
-});
